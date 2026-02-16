01b3ce082e8401d1c88fd4a75d1a85e42d71ffd0
Doppel_R <doppelr1001@gmail.com>
2026-02-16 12:18:32 +0100

    catalyst adding attempt

---
+++ src/main/java/net/doppelr/colorful_catalysts/CFCBlocks.java
@@ -0,0 +1,37 @@
+package net.doppelr.colorful_catalysts;
+
+import com.simibubi.create.AllTags;
+import com.simibubi.create.foundation.data.AssetLookup;
+import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.MapColor;
+
+import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
+import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
+import static net.doppelr.colorful_catalysts.CreateColorfulCatalysts.REGISTRATE;
+
+public class CFCBlocks {
+
+
+    // required: 16 blocks
+    // Order: white, light gray, gray, black, brown, red, orange, yellow, lime, green, cyan, light blue, blue, purple, magenta, pink
+    public static final BlockEntry<WrenchableBlock> WHITE_DYING_FAN_CATALYST = REGISTRATE.block("white_dying_fan_catalyst", WrenchableBlock::new)
+            .initialProperties(() -> Blocks.IRON_BLOCK)
+            .properties(p -> p
+                    .mapColor(MapColor.TERRACOTTA_YELLOW)
+                    .requiresCorrectToolForDrops()
+                    .noOcclusion()
+                    .lightLevel(s -> 10)
+                    .isRedstoneConductor((state, level, pos) -> false)
+            )
+            .addLayer(() -> RenderType::cutoutMipped)
+            .transform(pickaxeOnly())
+            .transform(FeatureToggle.registerDependent(CCBlocks.EMPTY_FAN_CATALYST))
+            .blockstate((c, p) -> p.simpleBlock(c.getEntry(), AssetLookup.partialBaseModel(c, p)))
+            .tag(AllTags.AllBlockTags.FAN_TRANSPARENT.tag)
+            .tag(AllTags.AllBlockTags.FAN_PROCESSING_CATALYSTS_BLASTING.tag)
+            .item()
+            .transform(customItemModel())
+            .register();
+}
