dd86ba81d8c86173cba09f489f42d08b645ebbb5
Doppel_R <doppelr1001@gmail.com>
2026-03-01 17:01:20 +0100

    preparing for concrete generation

---
+++ src/main/java/net/doppelr/colorful_catalysts/FluidInteraction.java
@@ -0,0 +1,10 @@
+package net.doppelr.colorful_catalysts;
+
+import com.simibubi.create.foundation.mixin.accessor.FluidInteractionRegistryAccessor;
+import net.neoforged.neoforge.common.NeoForgeMod;
+import net.neoforged.neoforge.fluids.FluidInteractionRegistry;
+import plus.dragons.createdragonsplus.common.fluids.dye.DyeFluidType;
+
+public class FluidInteraction {
+
+}
